cb9aff9e24bbf790536df935b345e9b212e58463
rajindersinghsodhi <rajsodhisocial@gmail.com>
2025-04-03 00:08:15 +0530

    Feature(user-controller): added user creation logic

--- controllers/users.js
+++ controllers/users.js
@@ -1,3 +1,39 @@
+import bcrypt from "bcryptjs";
+import { User } from "../models/user.js";
+
+const addUser = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const hashPassword = await bcrypt.hash(password, 10);
+
+        const result = await User.create({ name, email, password: hashPassword });
+        if(result._id){
+            return res.status(201).json(
+                {
+                    "status": "success",
+                    "message": "user created successfully"
+                }
+            );
+        }
+    } catch (error) {
+        if(error.code === 11000){
+            return res.status(409).json(
+                {
+                    "status": "error",
+                    "message": `email ${error.keyValue.email} is already used with different account`
+                }
+            )
+        }else{
+            return res.status(500).json(
+                {
+                    "status": "error",
+                    "message": error
+                }
+            )
+        }
+    }
+};
+
 const getUsers = (req, res) => {
     res.status(200).json(
         {
@@ -17,14 +53,6 @@ const getUser = (req, res) => {
 };
 
 
-const addUser = (req, res) => {
-    res.status(200).json(
-        {
-            "status": "success",
-            "message": "I will add data of requested user"
-        }
-    );
-};
 
 const updateUser = (req, res) => {
     res.status(200).json(
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import { userRoutes } from "./routes/users.js";
 const app = express();
 const server = createServer(app);
 
+app.use(express.json());
 app.use("/api/v1/users", userRoutes)
 
 export { app, server };
